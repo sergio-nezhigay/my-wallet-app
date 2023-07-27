0ccc8e31e1783c66ff16b5c2bd6af01440e58375
Sergio Nezhigay <abc1971h@gmail.com>
2023-07-27 20:36:54 +0300

    color correct

--- src/components/Connector/Connector.styled.ts
+++ src/components/Connector/Connector.styled.ts
@@ -9,6 +9,7 @@ export const Button = styled.button`
   border: 1px solid #f8f6f6;
   box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px,
     rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
+  color: inherit;
   background-color: #f8f6f6;
   transition: background-color 0.3s;
   &:hover,
